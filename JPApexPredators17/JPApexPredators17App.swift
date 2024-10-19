89a0473c7c45794d55fa536da0e6e3c07d9126c8
Karla <karlamiriam6@gmail.com>
2024-10-19 20:59:44 -0300

    Initial Commit

---
+++ JPApexPredators17/JPApexPredators17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators17App.swift
+//  JPApexPredators17
+//
+//  Created by Karla Miriam Dos santos Goncalves on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
